b964ee12403fc64b6bbd780d52e8426858d6325f
Jackie Gong <gongjackiegong@gmail.com>
2025-02-04 03:36:10 -0500

    feat: add tailscale VM for proxmox

---
+++ terraform/tailscale/vars.tf
@@ -0,0 +1,16 @@
+variable "proxmox_node" {
+  default = "hp-envy"
+}
+
+variable "gateway" {
+  default = "10.0.0.1"
+}
+
+variable "datastore" {
+  default = "local-zfs"
+}
+
+variable "user" {
+  default = "debian"
+}
+
