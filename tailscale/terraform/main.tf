d28aeb6aade970ee2796ee06e9ef78448d35e586
Jackie Gong <gongjackiegong@gmail.com>
2025-07-28 02:58:59 -0400

    fix: Update Tailscale setup to use Terraform Proxmox VM module.

---
+++ tailscale/terraform/main.tf
@@ -0,0 +1,55 @@
+module "debian" {
+  source = "../../terraform/vm/cloud-init"
+
+  providers = {
+    proxmox = proxmox
+  }
+
+  network = {
+    gateway = "10.0.2.1"
+    vlan    = 2
+    bridge  = "vmbr0"
+  }
+
+  nodes = {
+    "tailscale-00" = {
+      host_node    = "hp-envy"
+      description  = "Tailscale"
+      datastore = "local-zfs"
+      vm_id        = 150
+
+      cpu_cores    = 1
+      cpu_type     = "x86-64-v2-AES"
+
+      ram = {
+        dedicated = 768
+        floating  = 768
+      }
+
+      ip_address   = "10.0.2.150"
+      ip_mask      = 23
+
+      display = {
+        device = "socket"
+        vga    = "serial0"
+      }
+
+      disk = {
+        interface = "virtio0"
+        size      = 8 # in GiB
+      }
+    }
+  }
+
+  image = {
+    username           = "debian"
+    datastore          = "local"
+
+    content_type       = "import"
+    url                = "https://cloud.debian.org/images/cloud/bookworm/20250703-2162/debian-12-genericcloud-amd64-20250703-2162.qcow2"
+    file_name          = "debian-12-genericcloud-amd64-for_tailscale.qcow2"
+    checksum           = "da702efced2cd98017790d0e00fee81f1e1404d3f990a4741f52e6f18bde9856d37799c053b3baa48805048a595d2a6a13c41b8287ec6f76ec27b7ef1b67a215"
+    checksum_algorithm = "sha512"
+  }
+}
+
---
+++ tailscale/terraform/proxmox.auto.tfvars
@@ -0,0 +1,9 @@
+proxmox = {
+  name         = "hp-prodesk-600-1"
+  cluster_name = "homelab"
+  endpoint     = "https://proxmox.lan:8006/"
+  insecure     = true
+  username     = "root"
+  api_token    = "terraform@pve!provider=e73f8a19-403d-420d-94a7-7e85c2e792be"
+}
+
