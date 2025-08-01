074ea2a0a028dbfaaab940c70f670a06056a7eae
Jackie Gong <gongjackiegong@gmail.com>
2025-07-31 23:17:14 -0400

    feat: Add infrastructure creation and setup for Hashicorp Vault nodes.
    Creates an OpenTofu module for provisioning nodes for Hashicorp Vault
    and an Ansible playbook for bootstrapping packages and installation.

---
+++ vault/terraform/main.tf
@@ -0,0 +1,136 @@
+module "debian" {
+  source = "../../terraform/vm/cloud-init"
+
+  providers = {
+    proxmox = proxmox
+  }
+
+  network = {
+    gateway = "10.0.14.1"
+    vlan    = 14
+    bridge  = "vmbr0"
+  }
+
+  nodes = {
+    "hashicorp-vault-00" = {
+      host_node    = "beelink-eq14-1"
+      description  = "Hashicorp Vault Instance"
+      datastore    = "local-zfs"
+      vm_id        = 1410
+
+      cpu_cores    = 2
+      cpu_type     = "x86-64-v2-AES"
+
+      ram = {
+        dedicated = 2048
+        floating  = 2048
+      }
+
+      ip_address   = "10.0.14.10"
+      ip_mask      = 24
+
+      display = {
+        device = "socket"
+        vga    = "serial0"
+      }
+
+      disk = {
+        interface = "virtio0"
+        size      = 8 # in GiB
+      }
+    }
+    "hashicorp-vault-01" = {
+      host_node    = "beelink-eq14-2"
+      description  = "Hashicorp Vault Instance"
+      datastore    = "local-zfs"
+      vm_id        = 1411
+
+      cpu_cores    = 2
+      cpu_type     = "x86-64-v2-AES"
+
+      ram = {
+        dedicated = 2048
+        floating  = 2048
+      }
+
+      ip_address   = "10.0.14.11"
+      ip_mask      = 24
+
+      display = {
+        device = "socket"
+        vga    = "serial0"
+      }
+
+      disk = {
+        interface = "virtio0"
+        size      = 8 # in GiB
+      }
+    }
+    "hashicorp-vault-02" = {
+      host_node    = "hp-prodesk-600-1"
+      description  = "Hashicorp Vault Instance"
+      datastore    = "local-zfs"
+      vm_id        = 1412
+
+      cpu_cores    = 2
+      cpu_type     = "x86-64-v2-AES"
+
+      ram = {
+        dedicated = 2048
+        floating  = 2048
+      }
+
+      ip_address   = "10.0.14.12"
+      ip_mask      = 24
+
+      display = {
+        device = "socket"
+        vga    = "serial0"
+      }
+
+      disk = {
+        interface = "virtio0"
+        size      = 8 # in GiB
+      }
+    }
+    "hashicorp-vault-ops" = {
+      host_node    = "hp-prodesk-600-2"
+      description  = "Hashicorp Vault Instance"
+      datastore    = "local-zfs"
+      vm_id        = 1417
+
+      cpu_cores    = 2
+      cpu_type     = "x86-64-v2-AES"
+
+      ram = {
+        dedicated = 2048
+        floating  = 2048
+      }
+
+      ip_address   = "10.0.14.17"
+      ip_mask      = 24
+
+      display = {
+        device = "socket"
+        vga    = "serial0"
+      }
+
+      disk = {
+        interface = "virtio0"
+        size      = 8 # in GiB
+      }
+    }
+  }
+
+  image = {
+    username           = "debian"
+    datastore          = "local"
+
+    content_type       = "import"
+    url                = "https://cloud.debian.org/images/cloud/bookworm/20250703-2162/debian-12-genericcloud-amd64-20250703-2162.qcow2"
+    file_name          = "debian-12-genericcloud-amd64-for_hashicorp_vault.qcow2"
+    checksum           = "da702efced2cd98017790d0e00fee81f1e1404d3f990a4741f52e6f18bde9856d37799c053b3baa48805048a595d2a6a13c41b8287ec6f76ec27b7ef1b67a215"
+    checksum_algorithm = "sha512"
+  }
+}
+
