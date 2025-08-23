9dde156068df368923fa5588207a4d97783f103e
Jackie Gong <gongjackiegong@gmail.com>
2025-08-22 23:36:24 -0400

    chore: Use single certificate for entire OpenBao cluster.
    Using a single certificate for the OpenBao cluster for easier certificate management.

--- ca-cert/generate-openbao-certs.sh
+++ ca-cert/generate-openbao-certs.sh
@@ -15,31 +15,38 @@ function gen_csr() {
         return 1
     fi
 
-    local NODE_NAME="$1"
     local BASE_SUBJ="/C=US/ST=New York/L=New York/O=Homelab/OU=Homelab CA"
     local BASE_URL="openbao.internal.jackie.gg"
 
-    local COMMON_NAME="${NODE_NAME}.${BASE_URL}"
-    local SUBJECT_ALT_NAME
-    if [[ "$NODE_NAME" == node* ]]; then
-        SUBJECT_ALT_NAME="${BASE_URL}"
+    local PURPOSE
+    local COMMON_NAME
+    if [[ "$1" == node* ]]; then
+        PURPOSE="cluster"
+        COMMON_NAME="${BASE_URL}"
     else
-        SUBJECT_ALT_NAME="operator.${BASE_URL}"
+        PURPOSE="operator"
+        COMMON_NAME="operator.${BASE_URL}"
     fi
 
-    echo "Generating CSR for ${NODE_NAME}."
-    openssl req                                           \
-        -new                                              \
-        -key "${CERT_DIR}/${NODE_NAME}.key"               \
-        -subj "${BASE_SUBJ}/CN=${COMMON_NAME}"            \
-        -addext "subjectAltName = DNS:${SUBJECT_ALT_NAME}, DNS:${COMMON_NAME}" \
-        -out "${CERT_DIR}/openbao-${NODE_NAME}.csr"
+    local SUBJECT_ALT_NAME
+    SUBJECT_ALT_NAME="DNS:${COMMON_NAME}"
+    for arg in "$@"; do
+      SUBJECT_ALT_NAME+=", DNS:${arg}.${BASE_URL}"
+    done
+
+    echo "Generating CSR for ${PURPOSE}."
+    openssl req                                        \
+        -new                                           \
+        -key "${CERT_DIR}/${PURPOSE}.key"              \
+        -subj "${BASE_SUBJ}/CN=${COMMON_NAME}"         \
+        -addext "subjectAltName = ${SUBJECT_ALT_NAME}" \
+        -out "${CERT_DIR}/openbao-${PURPOSE}.csr"
 }
 
 function gen_crt() {
     # Check if a hostname was provided
     if [[ -z "$1" ]]; then
-        echo "Error: Missing node name (e.g., 'node-00')."
+        echo "Error: Missing purpose (e.g., 'cluster')."
         return 1
     fi
 
@@ -48,32 +55,28 @@ function gen_crt() {
         return 1
     fi
 
-    local NODE_NAME="$1"
+    local PURPOSE="$1"
     local INT_CA_KEY_PASS="$2"
     local INT_CA_CERT_PATH="${CA_CERT_DIR}/intermediate/ca.crt"
-    local LEAF_CERT_PATH="${CERT_DIR}/openbao-${NODE_NAME}.crt"
+    local LEAF_CERT_PATH="${CERT_DIR}/openbao-${PURPOSE}.crt"
 
-    echo "Generating certificate for ${NODE_NAME}."
+    echo "Generating certificate for ${PURPOSE}."
     openssl ca                                             \
         -config  "${CA_CERT_DIR}/intermediate/openssl.cnf" \
         -days 90                                           \
         -extensions svr_cert                               \
         -notext                                            \
         -passin "pass:${INT_CA_KEY_PASS}"                  \
-        -in "${CERT_DIR}/openbao-${NODE_NAME}.csr"         \
+        -in "${CERT_DIR}/openbao-${PURPOSE}.csr"         \
         -out "${LEAF_CERT_PATH}"
     
     # Create the cert bundle
-    cat "${LEAF_CERT_PATH}" "${INT_CA_CERT_PATH}" > "${CERT_DIR}/openbao-${NODE_NAME}.bundle.pem"
+    cat "${LEAF_CERT_PATH}" "${INT_CA_CERT_PATH}" > "${CERT_DIR}/openbao-${PURPOSE}.bundle.pem"
 }
 
-gen_csr "node-00"
-gen_csr "node-01"
-gen_csr "node-02"
+gen_csr "node-00" "node-01" "node-02"
 gen_csr "operator-00"
 
 read -s -p "Intermediate ca.key passphrase: " PASS
-gen_crt "node-00" "${PASS}"
-gen_crt "node-01" "${PASS}"
-gen_crt "node-02" "${PASS}"
-gen_crt "operator-00" "${PASS}"
+gen_crt "cluster" "${PASS}"
+gen_crt "operator" "${PASS}"
